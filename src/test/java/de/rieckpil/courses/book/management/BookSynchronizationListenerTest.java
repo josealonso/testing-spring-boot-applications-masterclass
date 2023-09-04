c10506a97c5d669fd971380b38fd85f4bb6a1ea6
josealonso <jose.alonso.programmer@gmail.com>
2023-09-04 18:56:08 +0200

    Skeleton for testing the BookSynchronizationListener class.

---
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -0,0 +1,26 @@
+package de.rieckpil.courses.book.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookSynchronizationListenerTest {
+
+  @Test
+  void shouldRejectWhenIsbnIsMalformed() {
+  }
+
+  @Test
+  void shouldNotOverrideWhenBookAlreadyExists() {
+  }
+
+  @Test
+  void shouldThrowExceptionWhenProcessingFails() {
+  }
+
+  @Test
+  void shouldStoreBookWhenNewAndCorrectIsbn() {
+  }
+
+
+}
