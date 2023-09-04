29d377c6ba7a078a20e4072a5bb5a6c47c41d87d
josealonso <jose.alonso.programmer@gmail.com>
2023-09-04 17:13:18 +0200

    First JUnit 5 file with lifecycle annotations.

---
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -0,0 +1,33 @@
+package de.rieckpil.courses.book.review;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewVerifierTest {
+
+  private ReviewVerifier reviewVerifier;
+
+  @BeforeEach
+  void setup() {
+    reviewVerifier = new ReviewVerifier();
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before All");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("After All");
+  }
+
+  @Test    // The method visibility does not really matter
+  void shouldFailWhenReviewContainsSwearWord() {
+    String review = "This book is shit";
+
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result);
+  }
+}
